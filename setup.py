2bbb902d56d8ff29f817c080c7bb1f4a8b9f23f9
himanshukgp <hs80941@gmail.com>
2017-10-24 20:15:32 +0530

    added LICENSE and setup.py files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='bdaybot',
+    version= ,
+    packages=['bdaybot'],
+    url='https://github.com/nitinkgp23/bdaybot',
+    license='',
+    author='Nitin Choudhary',
+    author_email='nitin.iitkgp23@gmail.com',
+    description='',
+    classifiers=[
+        'Development Status :: Beta',
+        'Intended Audience :: ',
+        'Topic :: ',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
